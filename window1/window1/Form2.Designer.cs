fec1c828988858c85d3ddc88d05202e681a42922
zied <zied.hack@gmail.com>
2019-05-27 18:49:24 +0100

    Form 2 with combo box automatically select in the database

--- window1/window1/Form2.Designer.cs
+++ window1/window1/Form2.Designer.cs
@@ -36,12 +36,98 @@ namespace window1
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.BackColor = System.Drawing.Color.Turquoise;
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(249, 55);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(150, 21);
+			this.comboBox1.TabIndex = 0;
+			this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1SelectedIndexChanged);
+			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(88, 55);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(100, 23);
+			this.label1.TabIndex = 1;
+			this.label1.Text = "titre";
+			// 
+			// label2
+			// 
+			this.label2.Location = new System.Drawing.Point(88, 113);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(100, 23);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "age";
+			// 
+			// label3
+			// 
+			this.label3.Location = new System.Drawing.Point(88, 203);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(100, 23);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "annee";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.YellowGreen;
+			this.textBox1.Location = new System.Drawing.Point(249, 113);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(202, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.Color.Yellow;
+			this.textBox2.Location = new System.Drawing.Point(249, 203);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(232, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(71, 251);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(117, 40);
+			this.button1.TabIndex = 6;
+			this.button1.Text = "Form 3";
+			this.button1.UseVisualStyleBackColor = true;
 			// 
 			// Form2
 			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.Text = "Form2";
+			this.ClientSize = new System.Drawing.Size(543, 320);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.comboBox1);
 			this.Name = "Form2";
+			this.Text = "Form2";
+			this.Load += new System.EventHandler(this.Form2Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
 		}
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
 	}
 }
--- window1/window1/Form2.cs
+++ window1/window1/Form2.cs
@@ -9,6 +9,8 @@
 using System;
 using System.Drawing;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data;
 
 namespace window1
 {
@@ -27,6 +29,43 @@ namespace window1
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+		}
+		
+		void ComboBox1SelectedIndexChanged(object sender, EventArgs e)
+		{
+		string StrCnn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Base.mdb"; //Création de l'objet de connexion
+            OleDbConnection  cnn = new OleDbConnection(StrCnn);
+            //ouverture de la connexion
+            cnn.Open();    
+            String req="SELECT  * FROM identif";
+                     OleDbCommand cmd=new OleDbCommand(req,cnn);
+                     OleDbDataAdapter DA =new OleDbDataAdapter();
+                     DA.SelectCommand=cmd;
+                     DataSet DS=new DataSet();
+                     DA.Fill(DS,"identif");
+                    textBox1.Text=DS.Tables["identif"].Rows[comboBox1.SelectedIndex]["age"].ToString();
+                    textBox2.Text=DS.Tables["identif"].Rows[comboBox1.SelectedIndex]["annee"].ToString();   
+			
+		}
+		
+		void Form2Load(object sender, EventArgs e)
+		{
+			string StrCnn = @"Provider=Microsoft.Jet.OLEDB.4.0;Data Source=Base.mdb"; //Création de l'objet de connexion
+            OleDbConnection  cnn = new OleDbConnection(StrCnn);
+            //ouverture de la connexion
+            cnn.Open();    
+            String req="SELECT  * FROM identif";
+                     OleDbCommand cmd=new OleDbCommand(req,cnn);
+                     OleDbDataAdapter DA =new OleDbDataAdapter();
+                     DA.SelectCommand=cmd;
+                     DataSet DS=new DataSet();
+                     DA.Fill(DS,"identif");
+                    
+                     comboBox1.DataSource=DS.Tables["identif"];
+                    comboBox1.DisplayMember="titre"; 
+                    textBox1.Text=DS.Tables["identif"].Rows[comboBox1.SelectedIndex]["age"].ToString();
+                    textBox2.Text=DS.Tables["identif"].Rows[comboBox1.SelectedIndex]["annee"].ToString();
+
 		}
 	}
 }
